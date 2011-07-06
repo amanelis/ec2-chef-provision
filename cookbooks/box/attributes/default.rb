58071681d8f4a4cdb164bdcfcce736c42904c256
Alex Manelis <amanelis@gmail.com>
2011-07-06 12:17:11 -0700

    First commit

--- cookbooks/box/attributes/default.rb
+++ cookbooks/box/attributes/default.rb
@@ -1,4 +1,6 @@
 default[:box][:user] = "box"
-default[:box][:skype_username] = ""
-default[:box][:skype_password] = ""
-default[:box][:authorized_keys] = ""
+default[:box][:skype_username] = "ermal14"
+default[:box][:skype_password] = "V7ps#RWeB34bT98#dpTd"
+default[:box][:authorized_keys] = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAusg0rH/pMbGOdeOjhgXWmzzDqc/uniVGH3CRLpAwBtAUXFxH415MVs3k5XyOvSz3Hq2/obD1qEKAxx6O5RYNiSEcg23bboI7rAThKiE4UvP0JVT9k1WPKwMd5RanCTOVTI6aCu7JnJsGp/LfhLUsaJpiBtkQP/J2bK8bpJ4TahrScBtCsMRTYmVWbbgfuNfQjBeUHlwlSp5EbNL5GXb7EtoHQN3y3WkOFPMGZcSVFV6mZIzbiLe+6QxJAxUmahFSLyiSlg78V5+yQ6aLy6YR9IJOELszhiPcXPQ0+H3/CSRyZwItUdh2XLxQIDwlpgcYJG3ZroDTZD2gc5FpLZzYBQ== martin@tubi.local"
+
+
